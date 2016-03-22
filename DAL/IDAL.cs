dd4d1a9a6d122fb62e8c02bb47cece94781922b2
Dov Berl <yonic.bk@gmail.com>
2016-03-22 16:48:02 +0200

    Project foundation-interfaces and references

---
+++ DAL/IDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedClasses;
+namespace DAL
+{
+    interface IDAL
+    {
+        List<User> RetrivAllUsersData();
+        User RetrivUserData(long ID);
+        bool UpdateUser(User use);
+        bool RegisterUser(User use);
+    }
+}
+
